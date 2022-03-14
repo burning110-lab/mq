4917ea626e9dec56d35e5e85c1ada6e87720be0b
quguangle <1123599561@qq.com>
2022-03-14 18:02:24 +0800

    修复依赖jar包安全的问题

--- rabbitmq/src/main/java/com/qu/lele/service/TestService.java
+++ rabbitmq/src/main/java/com/qu/lele/service/TestService.java
@@ -15,8 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.UUID;
 
 /**
